a875f8087e0276e14abccb92fb3de0899526e6f6
Nguyen Van Dung <121959862+cubi-dev@users.noreply.github.com>
2024-03-24 02:37:37 +0700

    Update auth.js

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "AIzaSyB4FJ-uS7TxiADUAzPReF76OD1p8lpeeKk";
+const API_KEY = "your API_KEY";
 
 async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
